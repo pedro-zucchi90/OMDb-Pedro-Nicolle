39ef6712cb83bffdd6ab8221fa853060254eb025
Pedro Zucchi <140649416+pedro-zucchi90@users.noreply.github.com>
2025-04-29 11:19:53 -0300

    Enchendo o db.py de comentários

--- db.py
+++ db.py
@@ -2,22 +2,20 @@ import psycopg
 from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 # Função para estabelecer uma conexão com o banco de dados
-def conexao():
+def get_connection():
     conn = psycopg.connect(
         dbname=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
         host=DB_HOST,
         port=DB_PORT
-    ) 
+    ) #estabelece uma conexão com o banco de dados utilizando as informações de configuração
     return conn
 
-
-
 # Função para iniciar o banco de dados, criando a tabela filmes_series SE NÃO EXISTIR
-def iniciar_db():
-    with conexao() as conn: 
-        with conn.cursor() as cur: 
+def init_db():
+    with get_connection() as conn: #abre uma conexão com o banco de dados
+        with conn.cursor() as cur: #abre um cursor para executar comandos SQL
             cur.execute("""
                 CREATE TABLE IF NOT EXISTS filmes_series (
                     id SERIAL PRIMARY KEY, 
@@ -36,5 +34,5 @@ def iniciar_db():
                     avaliacao_imdb TEXT,
                     votos_imdb TEXT
                 );
-            """)
-        conn.commit()
+            """) #executa um comando SQL para criar a tabela filmes_series se ela não existir
+        conn.commit()#confirma as alterações no banco de dados
