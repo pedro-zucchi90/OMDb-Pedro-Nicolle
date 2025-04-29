acb4867c3c9c120b4026076354b4dc9631a06d89
Pedro Zucchi <140649416+pedro-zucchi90@users.noreply.github.com>
2025-04-29 11:16:00 -0300

    Procurar nome ou id no OMDb
    Adicionei ao final: if __name__ == '__main__':
        init_db()
        app.run(debug=True) .
    Agora só umas "python app.py" para iniciar o projeto

--- app.py
+++ app.py
@@ -17,6 +17,36 @@ def buscar_por_nome():
             result = cur.fetchone()
             if result:
                 return jsonify(result[0])
+    # Se não achou no banco, busca na OMDB:
+    data = buscar_filme_nome(nome)
+    if data and data.get('Response') == 'True':
+        with conexao() as conn:
+            with conn.cursor() as cur:
+                cur.execute("""INSERT INTO filmes_series (imdb_id, titulo, ano, tipo, dados, total_temporadas, idioma, pais, premios, poster, avaliacoes, metascore, avaliacao_imdb, votos_imdb)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            ON CONFLICT (imdb_id) DO NOTHING;
+                        """, (
+                            data.get('imdbID'),
+                            data.get('Title'),
+                            data.get('Year'),
+                            data.get('Type'),
+                            json.dumps(data),  # dados gerais em json
+                            data.get('totalSeasons'),
+                            data.get('Language'),
+                            data.get('Country'),
+                            data.get('Awards'),
+                            data.get('Poster'),
+                            json.dumps(data.get('Ratings')),
+                            data.get('Metascore'),
+                            data.get('imdbRating'),
+                            data.get('imdbVotes')
+                        ))
+            conn.commit()
+        return jsonify(data)
+    else:
+        return jsonify({"erro": "filme ou série nao encontrado"}), 404
+
+#---------------------------------------------------------------------
 
 @app.route('/buscar/id', methods=['GET'])
 def buscar_por_id():
@@ -31,3 +61,36 @@ def buscar_por_id():
             result = cur.fetchone()
             if result:
                 return jsonify(result[0]) 
+
+        # Busca o filme ou série pela API do OMDB:
+    data = buscar_filme_id(imdb_id)
+    if data and data.get('Response') == 'True':
+        with conexao() as conn:
+            with conn.cursor() as cur:
+                cur.execute("""INSERT INTO filmes_series (imdb_id, titulo, ano, tipo, dados, total_temporadas, idioma, pais, premios, poster, avaliacoes, metascore, avaliacao_imdb, votos_imdb)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            ON CONFLICT (imdb_id) DO NOTHING;""", 
+                            (
+                            data.get('imdbID'),
+                            data.get('Title'),
+                            data.get('Year'),
+                            data.get('Type'),
+                            json.dumps(data),   
+                            data.get('totalSeasons'),
+                            data.get('Language'),
+                            data.get('Country'),
+                            data.get('Awards'),
+                            data.get('Poster'),
+                            json.dumps(data.get('Ratings')), 
+                            data.get('Metascore'),
+                            data.get('imdbRating'),
+                            data.get('imdbVotes')
+                        )) 
+            conn.commit() 
+        return jsonify(data) 
+    else:
+        return jsonify({"erro": "filme ou serie nao encontrado"}), 404
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True) 
