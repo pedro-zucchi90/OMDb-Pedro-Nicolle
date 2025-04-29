e1e3b2b49a38e0bd6a989250e733b107f1c26ef9
Pedro Zucchi <140649416+pedro-zucchi90@users.noreply.github.com>
2025-04-29 11:12:15 -0300

    Controle de segurança
    Caso uma requisição seja feita com nome inexistente ou com id inexistente

--- servico_omdb.py
+++ servico_omdb.py
@@ -5,10 +5,16 @@ from config import OMDB_API_KEY
 def buscar_filme_nome(nome):
     url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={nome}" 
     resposta = requests.get(url)
-    return resposta.json()
 
+    if resposta.status_code == 200:
+        return resposta.json()
+    return None 
+
+#buscar um filme ou série pelo id
 def buscar_filme_id(imdb_id):
     url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&i={imdb_id}" 
     resposta = requests.get(url)
-    return resposta.json()
-  
+
+    if resposta.status_code == 200:
+        return resposta.json()
+    return None 
